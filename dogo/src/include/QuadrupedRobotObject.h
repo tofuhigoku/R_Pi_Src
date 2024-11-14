29fb51d10ba3ead483834e516236abf3809c853e
khoa.vu-dang <khoa.vu-dang@banvien.com.vn>
2024-11-14 16:32:05 +0700

    update data_struct.h, QuadrupedRobotObject.h

--- dogo/src/include/QuadrupedRobotObject.h
+++ dogo/src/include/QuadrupedRobotObject.h
@@ -11,6 +11,9 @@
 
 #include <eigen3/Eigen/Dense>
 
+#include <glm/glm.hpp>
+#include <glm/trigonometric.hpp>
+
 #include "api.h"
 #include "data_struct.h"
 
@@ -18,67 +21,144 @@
 #define ENABLE_CMD			0XEC
 #define PHASE_ALIGN_CMD	    0XAC
 
-typedef struct				// size = 84 bytes
-{
-//    float q_abad[2];
-//    float q_hip[2];
-//    float q_knee[2];
-//    float qd_abad[2];
-//    float qd_hip[2];
-//    float qd_knee[2];
-	
-		leg_data_t leg1_data;	// 36 bytes
-		leg_data_t leg2_data;	// 36 bytes
-	
-    int32_t flags[2];	// 8 bytes
-    int32_t checksum;	// 4 bytes
-	
-} spi_data_t ;
 
+using namespace std;
 
-typedef struct	// size = 132 bytes
+typedef struct
 {
-//    float q_des_abad[2];
-//    float q_des_hip[2];
-//    float q_des_knee[2];
-//    float qd_des_abad[2];
-//    float qd_des_hip[2];
-//    float qd_des_knee[2];
-//    float kp_abad[2];
-//    float kp_hip[2];
-//    float kp_knee[2];
-//    float kd_abad[2];
-//    float kd_hip[2];
-//    float kd_knee[2];
-//    float tau_abad_ff[2];
-//    float tau_hip_ff[2];
-//    float tau_knee_ff[2];
+	/* data */
+	leg_data_t	leg1;
+	leg_data_t	leg2;
+	leg_data_t	leg3;
+	leg_data_t	leg4;
+
+	/* command */
+	leg_control_t leg1;
+	leg_control_t leg2;
+	leg_control_t leg3;
+	leg_control_t leg4;
 	
-		leg_control_t leg1_cmd;		// 60 bytes
-		leg_control_t leg2_cmd;		// 60 bytes
-	
-    int32_t flags[2];					// 8 bytes
-    uint32_t checksum;				// 4 bytes
-	
-} spi_command_t;
+} RobotLeg_t;
+
+typedef struct
+{
+    float L, W, OA, AB, BC;
+} RobotDimension_t;
+
+typedef struct
+{
+    float Ixx, Iyy, Izz;
+} Inertial_by_Axis_t;
+
+typedef struct
+{
+    float Body_Weight;
+
+    Inertial_by_Axis_t  body;
+
+    Inertial_by_Axis_t  Link11;
+    Inertial_by_Axis_t  Link12;
+    Inertial_by_Axis_t  Link13;
+
+    Inertial_by_Axis_t  Link21;
+    Inertial_by_Axis_t  Link22;
+    Inertial_by_Axis_t  Link23;
+
+    Inertial_by_Axis_t  Link31;
+    Inertial_by_Axis_t  Link32;
+    Inertial_by_Axis_t  Link33;
+
+    Inertial_by_Axis_t  Link41;
+    Inertial_by_Axis_t  Link42;
+    Inertial_by_Axis_t  Link43;
+
+} RobotInertial_param_t;
+
+typedef struct
+{
+    glm::mat3x3  body;
+
+    glm::mat3x3  Link1_hip;
+    glm::mat3x3  Link1_Uleg;
+    glm::mat3x3  Link1_Lleg;
+
+    glm::mat3x3  Link2_hip;
+    glm::mat3x3  Link2_Uleg;
+    glm::mat3x3  Link2_Lleg;
+
+    glm::mat3x3  Link3_hip;
+    glm::mat3x3  Link3_Uleg;
+    glm::mat3x3  Link3_Lleg;
+
+    glm::mat3x3  Link4_hip;
+    glm::mat3x3  Link4_Uleg;
+    glm::mat3x3  Link4_Lleg;
+
+} RobotInertial_matrix_t;
+
+typedef struct
+{
+    glm::vec3  body;
+
+    glm::vec3  Link1_hip;
+    glm::vec3  Link1_Uleg;
+    glm::vec3  Link1_Lleg;
+
+    glm::vec3  Link2_hip;
+    glm::vec3  Link2_Uleg;
+    glm::vec3  Link2_Lleg;
+
+    glm::vec3  Link3_hip;
+    glm::vec3  Link3_Uleg;
+    glm::vec3  Link3_Lleg;
+
+    glm::vec3  Link4_hip;
+    glm::vec3  Link4_Uleg;
+    glm::vec3  Link4_Lleg;
+
+} RobotCGpos_matrix_t;
+
 
 class QuadrupedRobotObject
 {
 private:
     /* data */
+    RobotDimension_t RobotDimension;
+    float weight;
+    RobotInertial_param_t   RobotInertial_param;
+    RobotInertial_matrix_t  RobotInertial_matrix;
+
+    glm::mat3 T_h1_11 = glm::mat3(1.0);
+    glm::mat3 T_h2_21 = glm::mat3(1.0);
+    glm::mat3 T_h3_31 = glm::mat3(1.0);
+    glm::mat3 T_h4_41 = glm::mat3(1.0);
+
+    /* private methods */
+    int R_RobotInertial_Init();
+    
+    int R_Leg_FK( glm::vec3 Vec3_LegJoints_Pos, glm::vec3 & Vec3_Foot_Pos);
+
+    int R_Leg_IK( glm::vec3 & Vec3_LegJoints_Pos, glm::vec3 Vec3_Foot_Pos);
+
 public:
     /* data */
+
     spi_command_t   spi_device0_cmd;
     spi_command_t   spi_device1_cmd;
 
     spi_data_t      spi_device0_data;
     spi_data_t      spi_device1_data;
 
-
+    RobotLeg_t RobotLeg;
 
     /* method */
     QuadrupedRobotObject(/* args */);
     ~QuadrupedRobotObject();
+
+    int R_All_Leg_FK();
+    int R_All_Leg_IK();
+
+    int R_Laydown();
 };
 
 QuadrupedRobotObject::QuadrupedRobotObject(/* args */)
@@ -89,5 +169,41 @@ QuadrupedRobotObject::~QuadrupedRobotObject()
 {
 }
 
+int QuadrupedRobotObject::R_Leg_FK( glm::vec3 Vec3_LegJoints_Pos, glm::vec3 & Vec3_Foot_Pos)
+{
+    float theta1 = Vec3_LegJoints_Pos[0];
+    float theta2 = Vec3_LegJoints_Pos[1];
+    float theta3 = Vec3_LegJoints_Pos[2];
 
+    float a00 =     glm::cos(theta1)*glm::cos(theta2+theta3);
+    float a01 = -1* glm::cos(theta1)*glm::sin(theta2+theta3);
+    float a02 = -1* glm::sin(theta1);
+    float a03 =     RobotDimension.AB* glm::cos(theta1)*glm::cos(theta2) - RobotDimension.OA*glm::sin(theta1) + RobotDimension.BC*glm::cos(theta1)*glm::cos(theta2+theta3);
+
+    float a10 =     glm::sin(theta1)*glm::cos(theta2+theta3);
+    float a11 = -1* glm::sin(theta1)*glm::sin(theta2+theta3);
+    float a12 =     glm::cos(theta1);
+    float a13 =     RobotDimension.AB* glm::sin(theta1)*glm::cos(theta2) + RobotDimension.OA*glm::cos(theta1) + RobotDimension.BC*glm::sin(theta1)*glm::cos(theta2+theta3);
+
+    float a20 = -1* glm::sin(theta2+theta3);
+    float a21 = -1* glm::cos(theta2+theta3);
+    float a22 =     0;
+    float a23 = -1* RobotDimension.AB*glm::sin(theta2) - RobotDimension.BC*glm::sin(theta2+theta3);
+
+    glm::mat4x4 T_11_14 = glm::transpose(glm::mat4x4{
+        {a00, a01, a02, a03},
+        {a10, a11, a12, a13},
+        {a20, a21, a22, a23},
+        {0  , 0  , 0  , 1  }      
+        });
+
+
+}
+
+int QuadrupedRobotObject::R_Leg_IK( glm::vec3 & Vec3_LegJoints_Pos, glm::vec3 Vec3_Foot_Pos)
+{
+
+
+
+}
 #endif
---
+++ dogo/temp_file/asin_lut_gen.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+
+
+#include "stdio.h"
+#include "stdlib.h"
+#include <stdint.h>
+#include "string.h"
+#include <errno.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#include <eigen3/Eigen/Dense>
+#include <glm/glm.hpp>
+#include <glm/trigonometric.hpp>
+#include <glm/gtx/io.hpp>
+#define GLM_ENABLE_EXPERIMENTAL
+#include <glm/gtx/string_cast.hpp>
+
+using namespace std;
+
+vector<float> asin_lut;
+
+int main(int argc, char** argv)
+{
+    // int lut_size = atoi(argv[1]);
+    // cout << "lut size = " << lut_size << endl;
+    // float step_size = 2.0f / lut_size;
+    // cout << "step size = " << step_size << endl;
+    // for (int i = 0; i < lut_size; i++)
+    // {
+    //     float var = -1 + step_size*i;
+    //     float a = glm::asin((double) var);
+    //     asin_lut.push_back( a);
+    //     cout << "glm::acos((double) " << var << " : " << asin_lut[i] << endl;
+    // }
+
+    glm::mat4 B{
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11},
+        {12, 13, 14, 15}};
+    // glm::acos( (double) x);
+    printf("hello world!\n");
+    cout << glm::transpose(B) << endl;
+    cout << glm::mat4(1.0) << endl;
+    return 0;
+}
